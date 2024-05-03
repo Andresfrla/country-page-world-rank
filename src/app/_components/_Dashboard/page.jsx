3949050cdb38b0ec57ca3e5f6adeae96920b884b
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-05-03 12:37:22 -0600

    fix the hidration

--- src/app/_components/_Dashboard/page.jsx
+++ src/app/_components/_Dashboard/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { formatNumber } from "@/app/_utils/_FormatNumber";
 import { fetchData } from "@/app/_utils/_UseApiData.js";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 
@@ -18,41 +19,41 @@ const DashBoard = () => {
   
 
   return (
-    <div>
-      <table className="w-full flex flex-col">
-        <thead className="mb-5">
-          <tr className="flex space-x-12 text-sm text-[#6C727F]">
-            <th className="w-[50px] text-left">Flag</th>
-            <th className="w-40 text-left">Name</th>
-            <th className="w-36 text-left">Population</th>
-            <th className="w-40 text-left">Area (km²)</th>
-            <th className="w-36 text-left">Region</th>
-          </tr>
-        </thead>
-        <tbody>
-          <hr className="border-[#6C727F]"/>
+    <TableContainer style={{ maxHeight: 'calc(100vh - 250px)', overflowY: 'auto' }}>
+      <Table className="w-full flex flex-col" stickyHeader aria-label="sticky table">
+        <TableHead className="mb-5" style={{ position: 'sticky', top: '0', zIndex: '999', background: '#fff' }}>
+          <TableRow className="flex space-x-12 text-sm text-[#6C727F]">
+            <TableCell className="w-[50px] text-left">Flag</TableCell>
+            <TableCell className="w-40 text-left">Name</TableCell>
+            <TableCell className="w-36 text-left">Population</TableCell>
+            <TableCell className="w-40 text-left">Area (km²)</TableCell>
+            <TableCell className="w-36 text-left">Region</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {/* <hr className="border-[#6C727F]"/> */}
           {data.map((country) => (
-            <tr key={country.name.common} className="flex space-x-12 items-center justify-center mt-4 font-medium text-[#D2D5DA] text-base">
-              <td className="w-[50px]"> 
+            <TableRow  key={country.name.common} className="flex space-x-12 items-center justify-center mt-4 font-medium text-[#D2D5DA] text-base">
+              <TableCell className="w-[50px]"> 
                 <img src={country.flags.png} style={{ width: '50px', height: '38px', objectFit: 'cover' }} className="rounded"/>
-              </td>
-              <td className="w-40">
-                <p>{country.name.common}</p>
-              </td>
-              <td className="w-36">
-                <p className="w-[472p36">{formatNumber(country.population)}</p>
-              </td>
-              <td className="w-40">
-                <p>{formatNumber(country.area)}</p>
-              </td>
-              <td className="w-36">
-                <p>{country.region}</p>
-              </td>
-            </tr>
+              </TableCell>
+              <TableCell className="w-40">
+                <Typography>{country.name.common}</Typography>
+              </TableCell>
+              <TableCell className="w-36">
+                <Typography className="w-[472p36">{formatNumber(country.population)}</Typography>
+              </TableCell>
+              <TableCell className="w-40">
+                <Typography>{formatNumber(country.area)}</Typography>
+              </TableCell>
+              <TableCell className="w-36">
+                <Typography>{country.region}</Typography>
+              </TableCell>
+            </TableRow>
           ))}
-        </tbody>
-      </table>
-    </div>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
