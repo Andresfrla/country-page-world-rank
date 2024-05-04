28f5f93842297cd411693227ee3126ed4b3011f3
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-05-04 11:38:15 -0600

    working on sort component

--- src/app/_components/_SideBar/_components/_Sort/page.jsx
+++ src/app/_components/_SideBar/_components/_Sort/page.jsx
@@ -1,18 +1,26 @@
 import * as React from 'react';
 import { Dropdown } from '@mui/base/Dropdown';
+import { MenuButton } from '@mui/base/MenuButton';
 import { Menu } from '@mui/base/Menu';
-import { MenuButton as BaseMenuButton, MenuButton } from '@mui/base/MenuButton';
-import { MenuItem as BaseMenuItem, menuItemClasses } from '@mui/base/MenuItem';
+import { MenuItem } from '@mui/base/MenuItem';
 
 const Sort = () => {
   return (
-    <div >
-        <h3 className='text-[#6C727F] text-sm'>Sort by</h3>
-        <Dropdown className="h-10 w-64 border border-[#282B30]">
-            <MenuButton className="flex flex-row gap-32 h-10 w-64 pl-3 mt-2 border border-[#6C727F] rounded-xl items-center font-bold text-[#D2D5DA] text-base">Population <img src="/Expand_down.svg" alt="Correct" className="size-4 ml-2" /></MenuButton>
-        </Dropdown>
+    <div>
+      <h3 className='text-[#6C727F] text-sm'>Sort by</h3>
+      <Dropdown className="h-10 w-64 border border-[#282B30]">
+        <MenuButton className="flex flex-row gap-32 h-10 w-64 pl-3 mt-2 border border-[#6C727F] rounded-xl items-center font-bold text-[#D2D5DA] text-base">
+          Population <img src="/Expand_down.svg" alt="Correct" className="size-4 ml-2" />
+        </MenuButton>
+        <Menu className="h-[110px] w-64 border p-3 border-[#282B30] bg-[#282B30] font-bold text-[#D2D5DA] rounded-xl">
+          <MenuItem>Name</MenuItem>
+          <MenuItem>Population</MenuItem>
+          <MenuItem>Area (km²)</MenuItem>
+          <MenuItem>Region</MenuItem>
+        </Menu>
+      </Dropdown>
     </div>
-  )
-}
+  );
+};
 
-export default Sort
+export default Sort;
