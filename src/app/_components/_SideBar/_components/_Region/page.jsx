fa2f10a9ae7af0c33d8cb21be59f42c0a3c99f55
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-05-04 11:21:44 -0600

    create the effect hover on the region

--- src/app/_components/_SideBar/_components/_Region/page.jsx
+++ src/app/_components/_SideBar/_components/_Region/page.jsx
@@ -1,17 +1,54 @@
 import React from 'react'
 
-const Region = () => {
+const Region = ({selectedRegions}) => {
+  console.log(selectedRegions)
 
   return (
     <div className='mt-12'>
         <h3 className='text-[#6C727F] text-sm mb-3'>Region</h3>
         <div className='flex flex-wrap w-64 gap-4'>
-            <button className='text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3 bg-[#282B30] rounded-xl'>Americas</button>
-            <button className='text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3'>Antarctic</button>
-            <button className='text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3'>Africa</button>
-            <button className='text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3'>Asia</button>
-            <button className='text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3'>Europe</button>
-            <button className='text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3'>Oceania</button>
+            <button 
+              className={`text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3 rounded-xl ${
+                selectedRegions.includes('Americas') ? 'bg-[#282B30]' : ''
+              }`}
+            >
+              Americas
+            </button>
+            <button 
+              className={`text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3 rounded-xl ${
+                selectedRegions.includes('Antarctic') ? 'bg-[#282B30]' : ''
+              }`}
+            >
+              Antarctic
+            </button>
+            <button 
+              className={`text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3 rounded-xl ${
+                selectedRegions.includes('Africa') ? 'bg-[#282B30]' : ''
+              }`}
+            >
+              Africa
+            </button>
+            <button 
+              className={`text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3 rounded-xl ${
+                selectedRegions.includes('Asia') ? 'bg-[#282B30]' : ''
+              }`}
+            >
+              Asia
+            </button>
+            <button 
+              className={`text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3 rounded-xl ${
+                selectedRegions.includes('Europe') ? 'bg-[#282B30]' : ''
+              }`}
+            >
+              Europe
+            </button>
+            <button 
+              className={`text-[#D2D5DA] text-base font-bold p-2 pl-3 pr-3 rounded-xl ${
+                selectedRegions.includes('Oceania') ? 'bg-[#282B30]' : ''
+              }`}
+            >
+              Oceania
+            </button>
         </div>
     </div>
   )
