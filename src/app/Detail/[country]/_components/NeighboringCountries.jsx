51fb322defbffda07bda494ac1f8a5b4e8b81a53
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2024-05-05 12:44:48 -0600

    add loader

--- src/app/Detail/[country]/_components/NeighboringCountries.jsx
+++ src/app/Detail/[country]/_components/NeighboringCountries.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getOneCountryByCode } from '@/app/_utils/_GetOneCountryByCode';
 import Link from 'next/link';
+import { Skeleton } from '@mui/material';
 
 const NeighboringCountries = ({ borders }) => {
     const [flags, setFlags] = useState({});
@@ -26,17 +27,42 @@ const NeighboringCountries = ({ borders }) => {
     return (
         <div className='flex flex-col w-full justify-start p-5'>
             <h2 className='text-xl font-bold text-[#6C727F] mb-5'>Neighboring Countries</h2>
-            <div className='flex flex-row gap-4'>
+            <div className='flex flex-row gap-4 overflow-x-auto custom-scrollbar items-start'>
                 {borders ? borders.map((countryCode) => (
                     <div key={countryCode} >
-                        <Link href={`/Detail/${countryNames[countryCode]}`}>
+                        <Link href={`/Detail/${countryNames[countryCode]}`} className='flex flex-col items-center text-center'>
                             <img 
-                            className='w-20 h-14 rounded-md mb-2'
+                            className='max-w-20 max-h-14 rounded-md mb-2 items-center'
                             src={flags[countryCode]} style={{ objectFit: 'cover' }} alt={`Flag of ${countryCode}`} />
-                            <span className="text-base ml-2">{countryNames[countryCode]}</span>
+                            <span className="text-base">{countryNames[countryCode]}</span>
                         </Link>
                     </div> 
-                )) : <h1>No borders</h1>}
+                )) : 
+                
+                <div className='flex gap-5'>
+                    <p className='flex w-28 text-center'>This country Don't have Neighboring Countries</p>
+                    <div>
+                        <Skeleton className='w-20 h-24 mt-[-20px] bg-[#6C727F] rounded-xl'>
+                        </Skeleton>
+                        <Skeleton className='w-20 h-7 mt-[-10px] bg-[#6C727F] rounded-xl'>
+                        </Skeleton>
+                    </div>
+                    <div>
+                        <Skeleton className='w-20 h-24 mt-[-20px] bg-[#6C727F] rounded-xl'>
+                        </Skeleton>
+                        <Skeleton className='w-20 h-7 mt-[-10px] bg-[#6C727F] rounded-xl'>
+                        </Skeleton>
+                    </div>
+                    <div>
+                        <Skeleton className='w-20 h-24 mt-[-20px] bg-[#6C727F] rounded-xl'>
+                        </Skeleton>
+                        <Skeleton className='w-20 h-7 mt-[-10px] bg-[#6C727F] rounded-xl'>
+                        </Skeleton>
+                    </div>
+                </div>
+                    
+
+                }
             </div>
         </div>
     );
